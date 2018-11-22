37d4b4fdd7e46017054294967f823d32a95c1ded
Mário Meyrelles <mariomeyrelles@hotmail.com>
2018-11-22 09:12:51 -0200

    Primeiro commit com o exemplo 1

---
+++ src/ModeloCozinhaIndustrial/ModeloCozinhaIndustrialExemplo1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ModeloCozinhaIndustrial
+{
+    class Program
+    {
+        public class Ingrediente
+        {
+            public string Nome;
+            public decimal Quantidade;
+        }
+
+        public class ItemCardapio { }
+
+        public class Ovo : Ingrediente { }
+        public class Manteiga : Ingrediente { }
+        public class Leite : Ingrediente { }
+        public class Sal : Ingrediente { }
+
+        public class OvoMexido : ItemCardapio { }
+
+        public class Cozinha
+        {
+            public OvoMexido PrepararOvoMexido(List<Ingrediente> ingredientes)
+            {
+                Console.WriteLine("Preparando ovo mexido" );
+                throw new NotImplementedException();
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/ModeloCozinhaIndustrial/XUnitTestProject1/PreparacaoOvoMexidoTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ModeloCozinhaIndustrialExemplo2
+{
+    public class PreparacaoOvoMexidoTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
