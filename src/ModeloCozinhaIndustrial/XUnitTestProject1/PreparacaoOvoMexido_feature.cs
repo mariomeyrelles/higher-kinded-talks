bd87908fa0e397760c7cfe371b4a2afe1549c4f2
Mário Meyrelles <mariomeyrelles@hotmail.com>
2018-11-25 19:41:21 -0200

    Montando uma primeira ideia do cenário para preparação do ovo mexido.

---
+++ src/ModeloCozinhaIndustrial/XUnitTestProject1/PreparacaoOvoMexido_feature.cs
@@ -0,0 +1,34 @@
+using LightBDD.Framework;
+using LightBDD.Framework.Scenarios.Basic;
+using LightBDD.Framework.Scenarios.Extended;
+using LightBDD.MsTest2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ModeloCozinhaIndustrialExemplo2
+{
+
+    [TestClass]
+    [FeatureDescription(
+@"In order to prepare an scrambled egg
+As an Chef
+I want to use the ingredients to prepare the dish")]
+    [Label("Story-1")]
+    public partial class PreparacaoOvoMexido_feature
+    {
+        [Scenario]
+        [Label("Scenario-1")]
+        public void Prepare_Scrambled_Eggs()
+        {
+            Runner.RunScenario(
+                _ => Given_kitchen_is_available(),
+                _ => Given_chef_is_available("Chef 1"),
+                _ => When_an_order_arrives_at_the_kitchen("Scrambled Egg"),
+                _ => Then_dish_must_be_prepared()
+             );
+        }
+
+
+
+    }
+}
---
+++ src/ModeloCozinhaIndustrial/XUnitTestProject1/PreparacaoOvoMexido_steps.cs
@@ -0,0 +1,35 @@
+using LightBDD.Framework;
+using LightBDD.Framework.Formatting;
+using LightBDD.Framework.Scenarios.Basic;
+using LightBDD.MsTest2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ModeloCozinhaIndustrialExemplo2
+{
+
+    
+    public partial class PreparacaoOvoMexido_feature : FeatureFixture
+    {
+
+
+        private void Given_kitchen_is_available()
+        {
+
+        }
+        private void Given_chef_is_available(string chefName)
+        {
+
+        }
+        private void When_an_order_arrives_at_the_kitchen(string order)
+        {
+
+        }
+        private void Then_dish_must_be_prepared()
+        {
+
+        }
+
+
+    }
+}
